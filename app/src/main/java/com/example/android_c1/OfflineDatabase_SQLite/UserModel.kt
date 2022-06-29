7627508cecd4e0064f2db053cb1e5cec2067400b
Mohit10-universe <mohitgujarati011@gmail.com>
2022-06-29 10:18:03 +0530

    27-6-2022 Curd SQLite Kt

---
+++ app/src/main/java/com/example/android_c1/OfflineDatabase_SQLite/UserModel.kt
@@ -0,0 +1,11 @@
+package mohit.dev.a27jun2022
+
+
+ class UserModel
+    (
+    var username: String,
+    var userid: String,
+    var userEmail: String,
+    var  userPassword: String,
+    var userMobile: String,
+){}
